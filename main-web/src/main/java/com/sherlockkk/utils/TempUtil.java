2b7c21eb70a0331dc43513c29fcc20529a59f7ba
SongJian <songjian0x00@163.com>
2018-09-29 16:57:22 +0800

    集成log4j2日志框架
    Signed-off-by: SongJian <songjian0x00@163.com>

--- main-web/src/main/java/com/sherlockkk/utils/TempUtil.java
+++ main-web/src/main/java/com/sherlockkk/utils/TempUtil.java
@@ -1,4 +1,12 @@
 package com.sherlockkk.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class TempUtil {
+    public static void main(String[] args) {
+        log.debug("--------------debug");
+        log.info("--------------info");
+        log.warn("-----------warn");
+    }
 }
